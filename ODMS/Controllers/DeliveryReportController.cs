340966a4dd2bbf74d2e034b85bd0573e49ef2ef1
tbldurbin <tbldurbin@gmail.com>
2018-08-05 15:10:19 +0600

    Version-2.0.6

---
+++ ODMS/Controllers/DeliveryReportController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using Microsoft.Reporting.WebForms;
+using ODMS.Models;
+
+namespace ODMS.Controllers
+{
+    [SessionExpire]
+    public class DeliveryReportController : Controller
+    {
+    public ODMSEntities Db = new ODMSEntities();
+     Supporting sp = new Supporting();
+        public ActionResult RptPsrWiseSkuWiseDelivery()
+        {
+            return View("PsrWiseSkuWiseDelivery/RptPsrWiseSkuWiseDelivery");
+        }
+
+        [HttpPost]
+
+        public ActionResult RptPsrWiseSkuWiseDeliveryFilter(int[] rsMid, int[] asMid, int[] cEid, int[] id, DateTime startDate, DateTime endDate)
+        {
+            HashSet<int> dbids = sp.Alldbids(rsMid, asMid, cEid, id);
+
+            ReportViewer reportViewer = new ReportViewer
+            {
+                ProcessingMode = ProcessingMode.Local,
+                SizeToReportContent = true,
+                Width = Unit.Percentage(100),
+                Height = Unit.Pixel(600)
+
+            };
+
+
+            List<RPT_Delivery_PSRWiseSKUWiseDelivery_Result> psrskudelivery = Db.RPT_Delivery_PSRWiseSKUWiseDelivery(startDate, endDate).Where(x => dbids.Contains(x.DB_Id)).ToList();
+
+            reportViewer.LocalReport.ReportPath = Request.MapPath(Request.ApplicationPath) + @"Reports\Delivery\RPT_PsrWiseSkuWiseDelivery.rdlc";
+
+            ReportDataSource rdc = new ReportDataSource("PSRSKUDELIVERY", psrskudelivery);
+
+            ReportParameter rp1 = new ReportParameter("DateParameter", startDate.ToString("dd-MMM-yyy") + " TO" + endDate.ToString("dd-MMM-yyy"));
+            ReportParameter rp2 = new ReportParameter("ReportNameParameter", "PSR SKU Wise Delivery [202]");
+
+            reportViewer.LocalReport.SetParameters(new[] { rp1, rp2 });
+
+            reportViewer.LocalReport.DataSources.Add(rdc);
+            
+            reportViewer.LocalReport.Refresh();
+            reportViewer.Visible = true;
+
+            ViewBag.ReportViewer = reportViewer;
+            return PartialView("PsrWiseSkuWiseDelivery/RptPsrWiseSkuWiseDeliveryFilter");
+        }
+
+
+        public ActionResult RptDeliveryKpi()
+        {
+            return View("kpi/RptDeliveryKpi");
+        }
+
+        [HttpPost]
+
+        public ActionResult RptDeliveryKpiFilter(int[] rsMid, int[] asMid, int[] cEid, int[] id, DateTime startDate, DateTime endDate)
+        {
+            HashSet<int> dbids = sp.Alldbids(rsMid, asMid, cEid, id);
+
+            ReportViewer reportViewer = new ReportViewer
+            {
+                ProcessingMode = ProcessingMode.Local,
+                SizeToReportContent = true,
+                Width = Unit.Percentage(100),
+                Height = Unit.Pixel(600)
+
+            };
+
+
+            List<RPT_Delivery_PSRPerformanceKPISummary_Result> psrkpisummary = Db.RPT_Delivery_PSRPerformanceKPISummary(startDate, endDate).Where(x => dbids.Contains(x.DB_Id)).ToList();
+
+            reportViewer.LocalReport.ReportPath = Request.MapPath(Request.ApplicationPath) + @"Reports\Delivery\RPT_PSRKPISummary.rdlc";
+
+            ReportDataSource rdc = new ReportDataSource("PSRKPISUMMARY", psrkpisummary);
+
+            ReportParameter rp1 = new ReportParameter("DateParameter", startDate.ToString("dd-MMM-yyy") + " TO " + endDate.ToString("dd-MMM-yyy"));
+            ReportParameter rp2 = new ReportParameter("ReportNameParameter", "PSR Wise KPI [203]");
+
+            reportViewer.LocalReport.SetParameters(new[] { rp1, rp2 });
+
+            reportViewer.LocalReport.DataSources.Add(rdc);
+
+            reportViewer.LocalReport.Refresh();
+            reportViewer.Visible = true;
+
+            ViewBag.ReportViewer = reportViewer;
+            return PartialView("Kpi/RptDeliveryKpiFilter");
+        }
+    }
+    }
---
+++ ODMS/HtmlHelpers/QrHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZXing;
+using ZXing.Common;
+
+namespace ODMS.HtmlHelpers
+{
+    public static class QrHelper
+    {
+        public static IHtmlString GenerateQrCode(this HtmlHelper html, string url, string alt = "QR code",int height = 100, int width = 100, int margin = 0)
+        {
+            var qrWriter = new BarcodeWriter
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Options = new EncodingOptions() { Height = height, Width = width, Margin = margin }
+            };
+
+            using (var q = qrWriter.Write(url))
+            {
+                using (var ms = new MemoryStream())
+                {
+                    q.Save(ms, ImageFormat.Png);
+                    var img = new TagBuilder("img");
+                    img.Attributes.Add("src",String.Format("data:image/png;base64,{0}", Convert.ToBase64String(ms.ToArray())));
+                    img.Attributes.Add("alt", alt);
+                    return MvcHtmlString.Create(img.ToString(TagRenderMode.SelfClosing));
+                }
+            }
+        }
+
+        public static byte[] GenerateQrCodeByteImage( string url)
+        {
+            //string alt = "QR code";
+            int height = 100;
+            int width = 100;
+            int margin = 0;
+
+            var qrWriter = new BarcodeWriter
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Options = new EncodingOptions() { Height = height, Width = width, Margin = margin }
+            };
+
+            using (var q = qrWriter.Write(url))
+            {
+                using (var ms = new MemoryStream())
+                {
+                    q.Save(ms, ImageFormat.Png);
+                    byte[] byteImage = ms.ToArray();
+                    return byteImage;
+                }
+            }
+        }
+      
+    }
+}
